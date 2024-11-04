626518769e886a7ce786f80559acb270370b0c3e
Sipoboy <alexrohmatullah1@gmail.com>
2024-11-04 20:24:54 +0700

    rawer

--- app/core/App.php
+++ app/core/App.php
@@ -1,10 +1,44 @@
 <?php
 
 class App {
+
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
-       var_dump($_GET);
+        $url = $this->parseURL();
+
+        // Cek apakah controller file ada?
+        if (empty($url)){
+            $this->controller;
+            $this->method;
+        }else {
+            if (file_exists('../app/controllers/'.$url[0].'.php')){
+                $this->controller = $url[0];
+                unset($url[0]);
+            }
+        }
+
+        // Panggil file Home yang ada pada folder controller
+        require_once '../app/controllers/'.$this->controller.'.php';
+        $this->controller = new $this->controller;
+
+        if (isset($url[1])){
+            if (method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        if (!empty($url)){
+            $this->params = array_values($url);
+        }
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
+    
     public function parseURL(){
         
         if (isset($_GET['url'])){
