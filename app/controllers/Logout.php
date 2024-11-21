68f64a084b3cbe076108f499b07645a524c47f03
GaniViola <ganiprasetyo711.@gmail.com>
2024-11-21 19:20:57 +0700

    update

--- app/controllers/Logout.php
+++ app/controllers/Logout.php
@@ -1,6 +1,9 @@
 <?php
+unset($_SESSION['login']);
+unset($_SESSION['EmailorUser']);
+unset($_SESSION['role']);
 session_destroy(); 
-setcookie('myKey', '', time() - 3600); 
-setcookie('key', '', time() - 3600);  
+setcookie('myKey', '', time() - 3600, '/'); 
+setcookie('key', '', time() - 3600, '/');  
 header('Location: '.BASEURL); 
 exit; 
